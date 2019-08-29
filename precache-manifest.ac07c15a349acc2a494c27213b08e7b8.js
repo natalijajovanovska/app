6aa538d9c1190b6677abab81b59cb3fea139fd9c
Natalija Jovanovska <natalija.j@live.com>
2019-08-29 14:09:32 +0200

    Updates

---
+++ precache-manifest.ac07c15a349acc2a494c27213b08e7b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe3607e5af4ebedadb15509a3d517fd",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "fa69b02626e2b96bead0",
+    "url": "/app/static/css/main.ba8533ec.chunk.css"
+  },
+  {
+    "revision": "898506ae59e02cea37b5",
+    "url": "/app/static/js/2.e204049d.chunk.js"
+  },
+  {
+    "revision": "fa69b02626e2b96bead0",
+    "url": "/app/static/js/main.3f68b1ce.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "c74554579e7f0bed04e79ca2d32827a0",
+    "url": "/app/static/media/logo.c7455457.png"
+  }
+]);
